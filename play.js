0e6a206854da9aff67c8f4f12a78e4c688dd1b4b
kapil kumar <kaps.logic@gmail.com>
2023-07-27 21:42:24 +0530

    Arrow functions

--- play.js
+++ play.js
@@ -1,11 +1,34 @@
-var name='Max';
-var age = 29;
-var hasHobbies = true;
+let name='Max';
+let age = 29;
+let hasHobbies = true;
 // console.log(name);
 
-function summarizeUser(userName, userAge, userHasHobbies){
+// function summarizeUser(userName, userAge, userHasHobbies){
+//     return 'Name: '+userName+', Age: '+userAge+', and the user has hobbies: '+userHasHobbies;
+// }
+
+// const summarizeUser = function(userName, userAge, userHasHobbies){
+//     return 'Name: '+userName+', Age: '+userAge+', and the user has hobbies: '+userHasHobbies;
+// };
+
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return 'Name: '+userName+', Age: '+userAge+', and the user has hobbies: '+userHasHobbies;
-}
+};
+
+// const sum = (a,b)=>{
+//   return (a+b);  
+// };
+
+const sum = (a,b)=> a+b;
+
+const addOne = (a)=> a+1
+
+const addRandom = ()=> 7+5
+
+
+console.log(sum(1,4))
+console.log(addOne(4))
+console.log(addRandom())
 
 console.log(summarizeUser(name,age,hasHobbies));
 
