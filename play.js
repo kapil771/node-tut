b9285b79592544abfa7ae88407aa6392df3b281a
kapil kumar <kaps.logic@gmail.com>
2023-07-27 21:03:09 +0530

    First Commit

---
+++ play.js
@@ -0,0 +1,11 @@
+var name='Max';
+var age = 29;
+var hasHobbies = true;
+// console.log(name);
+
+function summarizeUser(userName, userAge, userHasHobbies){
+    return 'Name: '+userName+', Age: '+userAge+', and the user has hobbies: '+userHasHobbies;
+}
+
+console.log(summarizeUser(name,age,hasHobbies));
+
